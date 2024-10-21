4616e297f8a35284c361b1e07da914b051580ff4
DimbelMe <167384693+DimbelMe@users.noreply.github.com>
2024-10-21 10:26:52 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
